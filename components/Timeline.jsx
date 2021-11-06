671a33d2da51ec0599f420da55d3361b57ed3def
Ambar Mutha <ambar.ytl@gmail.com>
2021-11-06 12:04:24 +0530

    fix: padding

--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -8,23 +8,19 @@ import { timelineData } from '../content/timeline';
 
 export function Timeline() {
 	return (
-		<>
-			<main className="container m-auto max-w-screen-md py-8">
-				<VerticalTimeline lineColor="black">
-					{timelineData.map(({ date, title, description }, idx) => (
-						<VerticalTimelineElement
-							key={idx}
-							iconClassName="bg-blue-600 text-white shadow-md"
-							textClassName="shadow-md"
-							icon={<StarIcon />}
-						>
-							<h3 className="text-xl">{title}</h3>
-							<h4 className="text-md">{date}</h4>
-							<p className="text-md font-normal">{description}</p>
-						</VerticalTimelineElement>
-					))}
-				</VerticalTimeline>
-			</main>
-		</>
+		<VerticalTimeline lineColor="black">
+			{timelineData.map(({ date, title, description }, idx) => (
+				<VerticalTimelineElement
+					key={idx}
+					iconClassName="bg-blue-600 text-white shadow-md"
+					textClassName="shadow-md"
+					icon={<StarIcon />}
+				>
+					<h3 className="text-xl">{title}</h3>
+					<h4 className="text-md">{date}</h4>
+					<p className="text-md font-normal">{description}</p>
+				</VerticalTimelineElement>
+			))}
+		</VerticalTimeline>
 	);
 }
