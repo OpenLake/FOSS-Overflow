4628fda023dce868ad6360d87e5c7e2ad7fcf049
Ambar Mutha <ambar.ytl@gmail.com>
2021-11-19 10:44:52 +0530

    Link preview icon

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
 				<meta property="og:title" content={title} />
 				<meta name="description" content={description} />
 				<meta property="og:description" content={description} />
-				<meta property="og:image" content={`${baseURL}/icon.jpg`} />
+				<meta property="og:image" content={`${baseURL}/icon.png`} />
 				<meta property="og:url" content={baseURL} />
 				<meta property="og:type" content="website" />
 				<meta name="twitter:card" content="summary_large_image" />
