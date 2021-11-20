3feab283c338adbba76eeb0c057c10618179aa82
KShivendu <shivendu@iitbhilai.ac.in>
2021-11-20 14:18:03 +0530

    projects: Add Activity Tracker and Leaderboard

--- content/projects.js
+++ content/projects.js
@@ -1,25 +1,25 @@
 export const projectData = [
 	{
-		title: 'Activity Tracker Extension',
+		title: 'Activity Tracker (Browser Extension)',
 		description:
-			'A cross-platform solution to discover how you spend your digital life. 🕵️‍♂️⌛',
+			'A chrome extension that tracks your browsing history and gives you insights into your browsing habits 🕵️‍♂️🌐',
 		wiki: 'https://github.com/OpenLake/Activity-Tracker/wiki/FOSS-Overflow-%7C-Web-Extension',
 		github: 'https://github.com/OpenLake/Activity-Tracker',
-		tags: ['web', 'nodejs'],
+		tags: ['browser-extension', 'javascript', 'nodejs'],
+	},
+	{
+		title: 'Activity Tracker (VS Code Extension)',
+		description:
+			'A VS code extension that tracks the amount of time your spend on each of your  projects. 👨‍💻⌛',
+		wiki: 'https://github.com/OpenLake/Activity-Tracker/wiki/FOSS-Overflow-%7C-VSCode-Extension',
+		github: 'https://github.com/OpenLake/Activity-Tracker',
+		tags: ['vscode-extension', 'web', 'javascript',],
 	},
 	{
 		title: 'Leaderboard Pro',
-		description: 'Leaderboard to stay consistent in programming 🥇',
+		description: 'A tool to keep track of the progress of you and your friends on coding platforms like Github, Codeforces, and CodeChef. 🥇',
 		wiki: 'https://github.com/OpenLake/Leaderboard-Pro/wiki/FOSS-Overflow',
 		github: 'https://github.com/OpenLake/Leaderboard-Pro',
-		tags: ['web', 'python'],
-	},
-	{
-		title: 'Activity Tracker',
-		description:
-			'A cross-platform solution to discover how you spend your digital life. 🕵️‍♂️⌛',
-		wiki: 'https://github.com/OpenLake/Activity-Tracker/wiki/FOSS-Overflow-%7C-VSCode-Extension',
-		github: 'https://github.com/OpenLake/Activity-Tracker',
-		tags: ['web', 'nodejs'],
+		tags: ['web', 'python', 'django'],
 	},
 ];
