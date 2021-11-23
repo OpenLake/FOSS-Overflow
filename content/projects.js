4a913bba976082a772015ae8e8f0d8cdfc6554d0
Ambar Mutha <ambar.ytl@gmail.com>
2021-11-23 12:51:01 +0530

    Add projects
    Co-authored-by: Kumar Shivendu <shivendu@iitbhilai.ac.in>

--- content/projects.js
+++ content/projects.js
@@ -13,13 +13,46 @@ export const projectData = [
 			'A VS code extension that tracks the amount of time your spend on each of your  projects. 👨‍💻⌛',
 		wiki: 'https://github.com/OpenLake/Activity-Tracker/wiki/FOSS-Overflow-%7C-VSCode-Extension',
 		github: 'https://github.com/OpenLake/Activity-Tracker',
-		tags: ['vscode-extension', 'web', 'javascript',],
+		tags: ['vscode-extension', 'web', 'javascript'],
 	},
 	{
 		title: 'Leaderboard Pro',
-		description: 'A tool to keep track of the progress of you and your friends on coding platforms like Github, Codeforces, and CodeChef. 🥇',
+		description:
+			'A tool to keep track of the progress of you and your friends on coding platforms like Github, Codeforces, and CodeChef. 🥇',
 		wiki: 'https://github.com/OpenLake/Leaderboard-Pro/wiki/FOSS-Overflow',
 		github: 'https://github.com/OpenLake/Leaderboard-Pro',
+		tags: ['web', 'python', 'django'],
+	},
+	{
+		title: 'Knowledge Sharing Platform',
+		description:
+			'A platform for college students to share course reviews, notes, and last year’s questions. 🎓📖',
+		wiki: 'https://github.com/OpenLake/Knowlege-Sharing-Platform/wiki/FOSS-Overflow',
+		github: 'https://github.com/OpenLake/Knowlege-Sharing-Platform',
+		tags: ['web', 'python', 'django'],
+	},
+	{
+		title: 'Photo Organizer',
+		description:
+			'Share photos📷 smartly where you just upload once and everyone gets their photos seperately 🤳🤖',
+		tags: ['web', 'python', 'django'],
+	},
+	{
+		title: 'Homework Scheduler',
+		description:
+			'Helps teachers decide homework deadlines without increasing load on the student 🧑‍🏫🧑‍🎓',
+		tags: ['web', 'nodejs'],
+	},
+	{
+		title: 'Quizz App',
+		description:
+			'A time bounded quiz app with realtime leaderboards and store of previous quizzes. 🧑‍🏫🧑‍🎓',
+		tags: ['web', 'python', 'django'],
+	},
+	{
+		title: 'Online Judge',
+		description:
+			'An online judge for programming competetions and assignments. 🧑‍💻📝',
 		tags: ['web', 'python', 'django'],
 	},
 	{
