1fae4c20c50eb62c5cff38ba5714c9f74e517c88
Ambar Mutha <ambar.ytl@gmail.com>
2021-11-28 12:31:17 +0530

    Update timeline

--- content/timeline.js
+++ content/timeline.js
@@ -5,34 +5,38 @@ export const timelineData = [
 	},
 	{
 		title: 'Application Period Ends',
-		date: '3 Dec, 2021',
+		date: '9 Dec, 2021',
 	},
 	{
 		title: "Students' Projects Announced",
-		date: '5 Dec, 2021',
+		date: '12 Dec, 2021',
+	},
+	{
+		title: 'Project Planning Period',
+		date: '13-19 Dec, 2021',
 	},
 	{
 		title: 'Coding Period Starts',
-		date: '6 Dec, 2021',
+		date: '20 Dec, 2021',
 	},
 	{
 		title: 'Mid Evaluations',
-		date: '19 Dec, 2021',
+		date: '2 Jan, 2021',
 	},
 	{
 		title: 'Coding Period Ends',
-		date: '2 Jan, 2022',
+		date: '16 Jan, 2022',
 	},
 	{
 		title: 'Students submit code and evaluations',
-		date: '6-8 Jan, 2022',
+		date: '20-22 Jan, 2022',
 	},
 	{
 		title: 'Mentor submits evaluations',
-		date: '9 Jan, 2022',
+		date: '23 Jan, 2022',
 	},
 	{
 		title: 'Results Announced',
-		date: '10 Jan, 2022',
+		date: '24 Jan, 2022',
 	},
 ];
