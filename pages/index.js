f652b0979afbd3cb0a60424b1c3543d362e11305
KShivendu <shivendu@iitbhilai.ac.in>
2021-12-04 17:35:45 +0530

    pages/index: Improve homepage and add discord link

--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,9 @@ export default function Index() {
 					<Image src={logoURL} alt="FOSS Overflow" />
 				</h1>
 				<p className="text-lg md:text-2xl">
-					A program by OpenLake and GDSC IIT Bhilai that helps students
-					understand the paradigm of Open Source contribution and gives them
-					real world software development experience.
+					FOSS Overflow is a program by OpenLake and GDSC IIT Bhilai that helps students to
+					get started with the world of open source by helping them build real world open sourced projects
+					under guidance of awesome mentors.
 				</p>
 			</div>
 
@@ -32,16 +32,17 @@ export default function Index() {
 					<h2>About</h2>
 
 					<p>
-						FOSS Overflow is a month-long festival celebrating open source and
-						innovation organized by Google Developer Student Clubs and OpenLake
-						at IIT Bhilai. Intensely sprawling over 4 weeks, it aims to give
-						hands-on experience of open source development to students with
-						little to medium level of experience on the subject. Students are
-						paired up with mentors to work on open sourced projects and amplify
+						FOSS Overflow is a month-long festival celebrating open source culture and is organized by
+						Google Developer Student Clubs and OpenLake at IIT Bhilai.
+					</p>
+					<p>
+						Students are paired up with mentors to work on open sourced projects and amplify
 						their skills and profile in the process while simultaneously bagging
-						cash awards and prizes. The endeavour is designed to ameliorate the
-						participants outlook and preparedness for global programs like GSoC,
-						LFX and Outreachy among others.
+						cash awards and prizes.
+					</p>
+					<p>
+						The program is designed to prepare students for global open source internships like GSoC,
+						LFX, and Outreachy that start shortly after the program is over.
 					</p>
 
 					<h2>Why should I apply?</h2>
@@ -57,23 +58,38 @@ export default function Index() {
 
 				<div className="prose lg:prose-xl mx-auto px-4">
 					<h2>How to Apply?</h2>
+					<ul>
+						<li>
+							The participants should write a maximum of 3 proposals for the projects that they wish to be a part of.
 
-					<p>
-						Students should write a proposal for a project they wish to be a
-						part of. Each student may submit upto three proposals. Proposals
-						must include what the student wants to implement and how they plan
-						to achieve it. The students are free to discuss with the project
-						mentors or organizing team while drafting their proposal. Ideal
-						proposals should also include tentative implementation details and
-						strategies to demonstrate that the applicant shall be able to
-						achieve what they are claiming.
-					</p>
+						</li>
+						<li>
+							Proposals must include what the participant wants to implement and
+							how they plan to achieve it.
+						</li>
+						<li>
+							The participants are free to discuss with the project mentors or the
+							organizing team while drafting their proposal.
+						</li>
+					</ul>
 
-					<Link href="/apply" passHref>
-						<a className="mx-auto w-max block bg-blue-800 no-underline text-white text-xl py-4 px-10 rounded-full focus-visible:ring-2 hover:ring-2 ring-blue-800 ring-offset-2">
-							Apply
-						</a>
-					</Link>
+					<div className="flex items-center justify-center flex-col md:flex-row">
+						<div>
+							<Link href="/apply" passHref>
+								<a className="mx-auto w-max block bg-gray-900 no-underline text-white text-xl py-4 px-10 rounded-full focus-visible:ring-2 hover:ring-2 ring-gray-800 ring-offset-2">
+									Apply
+								</a>
+							</Link>
+						</div>
+						<div>
+							<Link href="https://discord.gg/NavkU8fw" passHref>
+								<a className="mx-auto w-max block bg-blue-700 no-underline text-white text-xl py-4 px-10 rounded-full focus-visible:ring-2 hover:ring-2 ring-blue-800 ring-offset-2 my-5 mx-5">
+									Join Discord
+								</a>
+							</Link>
+						</div>
+					</div>
+
 
 					<h2>Eligibility</h2>
 
