3bbfd92614df45beef62bc3c74d7c0a8eb947828
Amay Dixit <65495380+amaydixit11@users.noreply.github.com>
2026-01-20 20:49:37 +0530

    Update timeline.js

--- content/timeline.js
+++ content/timeline.js
@@ -1,50 +1,50 @@
 export const timelineData = [
 	{
 		title: "Event Inauguration",
-		date: '18 Jan, 2026',
+		date: '19 Jan, 2026',
 	},
 	{
 		title: 'Application Period Starts',
-		date: '18 Jan, 2026',
+		date: '20 Jan, 2026',
 	},
 	{
 		title: 'Application Period Ends',
-		date: '28 Jan, 2026',
+		date: '30 Jan, 2026',
 	},
 	{
 		title: "Annoncement of Selected Mentees",
-		date: '30 Jan, 2026',
+		date: '31 Jan, 2026',
 	},
 	{
 		title: "Bonding Period Starts",
-		date: '30 Jan, 2026',
+		date: '1 Feb, 2026',
 	},
 	{
 		title: 'Bonding Period Ends',
-		date: '2 Feb, 2026',
-	},
-	{
-		title: 'Coding Period Start',
 		date: '3 Feb, 2026',
 	},
 	{
+		title: 'Coding Period Start',
+		date: '4 Feb, 2026',
+	},
+	{
 		title: 'Mid Evaluations',
-		date: '18 Feb, 2026',
+		date: '22 Feb, 2026',
 	},
 	{
 		title: 'Mid Evaluations Results',
-		date: '20 Feb, 2026',
+		date: '24 Feb, 2026',
 	},
 	{
 		title: 'Coding Period Ends',
-		date: '6 Mar, 2026',
+		date: '18 Mar, 2026',
 	},
 	{
 		title: 'Final Evaluation Submission',
-		date: '6 Mar, 2026',
+		date: '18 Mar, 2026',
 	},
 	{
 		title: 'Results Announced',
-		date: '12 Mar, 2026',
+		date: '20 Mar, 2026',
 	},
 ];
